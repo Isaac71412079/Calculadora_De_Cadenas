23c4b5e770e305c0e10c9e364df601c8428586a6
Isaac71412079 <isaac.rivero@ucb.edu.co>
2024-05-13 19:01:03 -0400

    Implementación inicial: devuelve 0 para cadena vacía

---
+++ src/calculadorDeCadenas.spec.js
@@ -0,0 +1,10 @@
+describe("Calcular Cadenas", () => {
+    it("Implementación inicial: devuelve 0 para cadena vacía", () => {
+        let cadena = "";
+      expect(calcularCadena(cadena)).toEqual(0);
+    });
+  });
+
+  function calcularCadena(cadena){
+    return 0;
+  }

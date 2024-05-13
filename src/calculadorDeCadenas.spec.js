0a5c5d099fa7edd308dc7462fa118eabb906c16e
Isaac71412079 <isaac.rivero@ucb.edu.co>
2024-05-13 19:06:33 -0400

    Se devuelve 0 al no insertar niguna cadena en la función

--- src/calculadorDeCadenas.spec.js
+++ src/calculadorDeCadenas.spec.js
@@ -1,10 +1,14 @@
+import calcularCadena from "./calculadorDeCadenas";
+
 describe("Calcular Cadenas", () => {
     it("Implementación inicial: devuelve 0 para cadena vacía", () => {
         let cadena = "";
       expect(calcularCadena(cadena)).toEqual(0);
     });
+
+    it("Se devuelve 0 al no insertar niguna cadena en la función", () => {
+      expect(calcularCadena()).toEqual(0);
+    });
   });
 
-  function calcularCadena(cadena){
-    return 0;
-  }
+  
